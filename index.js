b218ac8e65381b08ba8eeea5adc24575619ebaa8
Carolina2301 <carolmidory@gmail.com>
2023-05-18 02:38:57 -0300

    responsivo

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+window.addEventListener('load', function() {
+  var logo = document.getElementById('logo');
+  
+  if (window.innerWidth <= 768) {
+    logo.src = 'images/ComuniCards-mobile.svg';
+  }
+});
+
+
 document.addEventListener("DOMContentLoaded", function() {
   const novoJogoLink = document.getElementById("novoJogoLink");
 
@@ -8,13 +17,18 @@ document.addEventListener("DOMContentLoaded", function() {
       title: 'Selecione um deck de cartas:',
       showCancelButton: true,
       showDenyButton: true,
-      confirmButtonText: '<div><img src="https://carolina2301.github.io/images/emocao.svg" alt="Imagem"></div><div>Emoções</div>',
-      denyButtonText: '<div><img src="https://carolina2301.github.io/images/comida.svg" alt="Imagem"></div><div>Comidas</div>',
+      confirmButtonText: '<div><img src="https://carolina2301.github.io/images/emocao.svg" alt="Imagem" class="button-image"></div><div>Emoções</div>',
+      denyButtonText: '<div><img src="https://carolina2301.github.io/images/comida.svg" alt="Imagem" class="button-image"></div><div>Comidas</div>',
       cancelButtonText: 'Voltar',
       reverseButtons: true,
       confirmButtonColor: '#2AB7CA',
       denyButtonColor: '#2AB7CA',
       cancelButtonColor: '#2AB7CA',
+      customClass: {
+        confirmButton: 'custom-sawl-confirmButton',
+        denyButton: 'custom-sawl-denyButton',
+
+      },
     }).then((result) => {
       if (result.isConfirmed) {
         window.location.href = 'https://carolina2301.github.io/fases/fase1/emocoes_fase1.html';
